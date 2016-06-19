17f6e2ff9c6578197263585274a632c653391a23
Kevin Fan <gokevinan@gmail.com>
2016-06-19 11:58:24 +0800

    A very simple tcp server which will respond with `HelloWorld` whenever a reqeust is comming in.

--- lib/server.rb
+++ lib/server.rb
@@ -2,14 +2,29 @@ require 'socket'
 
 module RIFS
   class Server
+
+    # The tcp server
+    attr_reader :server
+
     def initialize(port=8088, opts={})
+      @server = TCPServer.new port
+
+      trap(:INT) { stop }
+      puts "Serving at port #{port}"
     end
 
     def start
+      loop do
+        sock = server.accept
+        sock.puts 'Hello world'
+        sock.close
       end
     end
 
     def stop
+      puts 'Shutting down...'
+      server.close
+      puts 'Stoped!'
     end
   end
 end
