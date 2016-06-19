8d00bd1bf6e4f98ff9439add66a0f7d2ca2f61dd
Kevin Fan <gokevinan@gmail.com>
2016-06-19 12:00:01 +0800

    Serving mutilpe requests at the same time.
    Creating a new thread to server a new request each time.

--- lib/server.rb
+++ lib/server.rb
@@ -2,29 +2,35 @@ require 'socket'
 
 module RIFS
   class Server
-
     # The tcp server
     attr_reader :server
 
     def initialize(port=8088, opts={})
       @server = TCPServer.new port
 
-      trap(:INT) { stop }
-      puts "Serving at port #{port}"
+      trap(:INT) {stop}
+
+      puts "Serving on #{port}"
     end
 
     def start
       loop do
         sock = server.accept
-        sock.puts 'Hello world'
-        sock.close
+        Thread.new {handle_request sock}
       end
     end
 
+    def handle_request(sock)
+        sock.puts 'Hello world'
+        sleep 3
+        sock.puts 'Hello world 2'
+        sock.close
+    end
+
     def stop
-      puts 'Shutting down...'
+      puts 'Shutting down....'
       server.close
-      puts 'Stoped!'
+      puts 'Server stopped!'
     end
   end
 end
