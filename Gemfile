3ba9f24ac8233cbb888782b166c4ace0a5187b8d
Kevin Fan <gokevinan@gmail.com>
2016-06-19 12:17:07 +0800

    Add status to server and also ignore EBADF error.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,7 @@
 # frozen_string_literal: true
 source "https://rubygems.org"
 
+group :dev do
+  gem 'byebug'
+end
+
--- lib/server.rb
+++ lib/server.rb
@@ -5,19 +5,30 @@ module RIFS
     # The tcp server
     attr_reader :server
 
+    # The status of the server
+    attr_accessor :status
+
     def initialize(port=8088, opts={})
       @server = TCPServer.new port
 
       trap(:INT) {stop}
 
+      self.status = :NEW
       puts "Serving on #{port}"
     end
 
     def start
+      self.status = :RUNNING
       loop do
+        break unless running?
         sock = server.accept
         Thread.new {handle_request sock}
       end
+    rescue Errno::EBADF, Errno::ENOTSOCK, IOError => ex
+    end
+
+    def running?
+      status == :RUNNING
     end
 
     def handle_request(sock)
@@ -28,8 +39,10 @@ module RIFS
     end
 
     def stop
+      status = :SHUTTING_DOWN
       puts 'Shutting down....'
       server.close
+      status = :STOPPED
       puts 'Server stopped!'
     end
   end
