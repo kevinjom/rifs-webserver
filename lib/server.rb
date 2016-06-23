be12883dd5789a72c556b519253397347f1c65d8
kevinjom <gokevinvan@gmail.com>
2016-06-23 16:43:00 +0800

    Response contains status_line, headers and body, so write them in order.

--- lib/server.rb
+++ lib/server.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 require 'socket'
 
 module RIFS
   class Server
+    SERVER_NAME = 'rifs-webserver'
+
     # The tcp server
     attr_reader :server
 
@@ -32,10 +36,24 @@ module RIFS
     end
 
     def handle_request(sock)
-        sock.puts 'Hello world'
-        sleep 3
-        sock.puts 'Hello world 2'
+        write_status_line(sock)
+        write_headers(sock)
+        write_body(sock)
         sock.close
+    end
+
+    def write_status_line(sock)
+      sock.puts 'HTTP/1.1 200 OK'
+    end
+
+    def write_body(sock)
+        sock.puts 'Hello world'
+        sock.puts 'Hello world 2'
+    end
+
+    def write_headers(sock)
+      sock.puts "Server: #{SERVER_NAME}"
+      sock.puts ''
     end
 
     def stop
